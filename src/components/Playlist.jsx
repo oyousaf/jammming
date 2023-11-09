e263a3eeb61b35dc8d683c08fb83c91782d91d9b
Omar ♔ <o_yousaf@live.co.uk>
2023-11-09 20:05:39 +0000

    refactor

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -3,13 +3,14 @@ import { authConfig } from "../constants/authConfig";
 import Playlists from "./Playlists";
 
 const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
-  const [error, setError] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [selectedPlaylist, setSelectedPlaylist] = useState(null);
+  const [error, setError] = useState(null);
+  const [playlistId, setPlaylistId] = useState(null);
 
-  // Grabbing the access token from the URL
+  // Spotify callback handling
   const handleSpotifyCallback = () => {
     const token = new URLSearchParams(window.location.hash.substring(1)).get(
       "access_token"
@@ -27,6 +28,7 @@ const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
     }
   };
 
+  // Fetch user data from Spotify API
   const fetchUserData = async () => {
     try {
       const response = await fetch("https://api.spotify.com/v1/me", {
@@ -46,7 +48,7 @@ const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
     }
   };
 
-  // Function to refresh the access token when it expires
+  // Refresh Spotify access token
   const refreshAccessToken = async () => {
     try {
       const refreshToken = localStorage.getItem("spotify_refresh_token");
@@ -83,8 +85,8 @@ const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
     }
   };
 
+  // useEffect to handle Spotify callback, fetch user data, and refresh access token
   useEffect(() => {
-    // Handle Spotify callback and fetch user data if the access token exists
     handleSpotifyCallback();
 
     if (accessToken) {
@@ -98,18 +100,16 @@ const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
     // eslint-disable-next-line
   }, [accessToken]);
 
+  // Initiate Spotify login
   const initiateSpotifyLogin = () => {
     const { clientId, redirectUri, scope } = authConfig;
     window.location.href = `${authConfig.authorizationUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
   };
 
+  // Function to save playlist to Spotify
   const savePlaylistToSpotify = async () => {
     if (!accessToken || !playlist.tracks.length) {
-      setError(
-        <p className="mt-5 text-red-600">
-          Playlist cannot be empty! Try adding some tracks.
-        </p>
-      );
+      setError("Playlist cannot be empty! Try adding some tracks.");
       return;
     }
 
@@ -124,42 +124,62 @@ const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
         public: false,
       };
 
-      const createPlaylistResponse = await fetch(
-        "https://api.spotify.com/v1/me/playlists",
-        {
-          method: "POST",
-          headers,
-          body: JSON.stringify(playlistData),
+      if (playlistId) {
+        const updatePlaylistResponse = await fetch(
+          `https://api.spotify.com/v1/users/${user.id}/playlists/${playlistId}`,
+          {
+            method: "PUT",
+            headers,
+            body: JSON.stringify(playlistData),
+          }
+        );
+        if (!updatePlaylistResponse.ok) {
+          setError("Error updating the playlist.");
+          return;
         }
-      );
-
-      if (createPlaylistResponse.ok) {
-        const { id } = await createPlaylistResponse.json();
-        const uris = playlist.tracks.map((track) => track.uri);
-
-        const addTracksResponse = await fetch(
-          `https://api.spotify.com/v1/playlists/${id}/tracks`,
+      } else {
+        const createPlaylistResponse = await fetch(
+          "https://api.spotify.com/v1/me/playlists",
           {
             method: "POST",
             headers,
-            body: JSON.stringify({ uris }),
+            body: JSON.stringify(playlistData),
           }
         );
 
-        if (addTracksResponse.ok) {
-          alert("Playlist saved to Spotify!");
-          onEdit([]);
+        if (createPlaylistResponse.ok) {
+          const { id } = await createPlaylistResponse.json();
+          setPlaylistId(id);
         } else {
-          setError("Error adding tracks to the playlist.");
+          setError("Error creating playlist.");
+          return;
         }
+      }
+
+      const uris = playlist.tracks.map((track) => track.uri);
+
+      const addTracksResponse = await fetch(
+        `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+        {
+          method: "POST",
+          headers,
+          body: JSON.stringify({ uris }),
+        }
+      );
+
+      if (addTracksResponse.ok) {
+        alert("Playlist saved to Spotify!");
+        onEdit([]);
+        setPlaylistId(null);
       } else {
-        setError("Error creating the playlist.");
+        setError("Error adding tracks to the playlist.");
       }
     } catch (error) {
       setError(`Error saving playlist to Spotify: ${error.message}`);
     }
   };
 
+  // Function to play a sample of the track
   const playTrackSample = (track) => {
     if (track.preview_url) {
       const audio = new Audio(track.preview_url);
@@ -170,13 +190,13 @@ const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
     }
   };
 
-  // Function to handle the selection of a playlist from Playlists component
+  // Function to handle the selection of a playlist
   const handleSelectPlaylist = (selectedPlaylist) => {
     console.log("Selected Playlist in Playlist.jsx:", selectedPlaylist);
     setSelectedPlaylist(selectedPlaylist);
   };
 
-  // Function to render playlist tracks or the selectedPlaylist
+  // Determine which tracks to display based on the selected playlist or the main playlist
   const tracksToDisplay = selectedPlaylist
     ? selectedPlaylist.tracks || []
     : playlist.tracks || [];
@@ -192,74 +212,70 @@ const Playlist = ({ playlist, name, onEdit, onSave, onRemove }) => {
         }}
         onBlur={onSave}
       />
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {tracksToDisplay.map((track) => (
-            <div
-              className="flex justify-center h-7 text-[#003636] text-[1rem] border-b border-gray-300"
-              key={track.id}
+      <ul>
+        {tracksToDisplay.map((track) => (
+          <div
+            className="flex justify-center h-7 text-[#003636] text-[1rem] border-b border-gray-300"
+            key={track.id}
+          >
+            <li>
+              {track.name} - {track.artist}
+            </li>
+            <button
+              className="text-teal-500 hover:text-teal-600 font-bold pl-2"
+              onClick={() => playTrackSample(track)}
             >
-              <li>
-                {track.name} - {track.artist}
-              </li>
-              <button
-                className="text-teal-500 hover:text-teal-600 font-bold pl-2"
-                onClick={() => playTrackSample(track)}
-                aria-label="Play track sample"
-              >
-                ▷
-              </button>
-              <button
-                className="text-red-600 hover:text-red-500 font-bold pl-2 flex-end"
-                onClick={() => onRemove(track)}
-                aria-label="Remove track"
-              >
-                x
-              </button>
-            </div>
-          ))}
-        </ul>
-      )}
-      {accessToken ? (
-        <div className="mt-10 text-green-700">
-          {user ? (
-            <>
-              <div className="flex items-center justify-center">
-                <p className="mr-2">Logged in as {user.display_name}</p>
-                <img
-                  className="rounded-full"
-                  src={user.images[0].url}
-                  alt="User profile"
-                  height={30}
-                  width={30}
+              ▷
+            </button>
+            <button
+              className="text-red-600 hover:text-red-500 font-bold pl-2 flex-end"
+              onClick={() => onRemove(track)}
+            >
+              x
+            </button>
+          </div>
+        ))}
+      </ul>
+      {!loading ? (
+        accessToken ? (
+          <div className="mt-10 text-green-700">
+            {user ? (
+              <>
+                <div className="flex items-center justify-center">
+                  <p className="mr-2">Logged in as {user.display_name}</p>
+                  <img
+                    className="rounded-full"
+                    src={user.images[0]?.url}
+                    alt="User profile"
+                    height={30}
+                    width={30}
+                  />
+                </div>
+                <button
+                  className="font-bold text-white hover:text-[#6c41e9] bg-[#6c41e9] hover:bg-[lightgrey] py-[.57rem] px-2 mt-10 rounded-[54px] transition 0.25s"
+                  onClick={savePlaylistToSpotify}
+                >
+                  SAVE PLAYLIST
+                </button>
+                <Playlists
+                  accessToken={accessToken}
+                  playlist={playlist}
+                  onSelectPlaylist={handleSelectPlaylist}
                 />
-              </div>
-              <button
-                className="font-bold text-white hover:text-[#6c41e9] bg-[#6c41e9] hover:bg-[lightgrey] py-[.57rem] px-2 mt-10 rounded-[54px] transition 0.25s"
-                onClick={savePlaylistToSpotify}
-              >
-                SAVE PLAYLIST
-              </button>
-              <Playlists
-                accessToken={accessToken}
-                playlist={playlist}
-                onSelectPlaylist={handleSelectPlaylist}
-              />
-            </>
-          ) : (
-            <p>Loading user data...</p>
-          )}
-        </div>
-      ) : (
-        <button
-          className="font-bold text-gray-400 hover:text-white bg-purple-200 hover:bg-[#6c41e9] uppercase py-[.57rem] px-2 mt-10 rounded-[54px] transition 0.25s"
-          onClick={initiateSpotifyLogin}
-        >
-          Login to Spotify
-        </button>
-      )}
+              </>
+            ) : (
+              <p>Loading user data...</p>
+            )}
+          </div>
+        ) : (
+          <button
+            className="font-bold text-gray-400 hover:text-white bg-purple-200 hover:bg-[#6c41e9] uppercase py-[.57rem] px-2 mt-10 rounded-[54px] transition 0.25s"
+            onClick={initiateSpotifyLogin}
+          >
+            Login to Spotify
+          </button>
+        )
+      ) : null}
       {error && <p className="text-red-600">{error}</p>}
     </div>
   );
