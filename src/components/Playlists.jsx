5706d4a56d21d3063ae3e4e07d9c70d30cf901d6
Omar ♔ <o_yousaf@live.co.uk>
2023-11-10 16:01:34 +0000

    refactored

--- src/components/Playlists.jsx
+++ src/components/Playlists.jsx
@@ -3,22 +3,18 @@ import PlaylistsItem from "./PlaylistsItem";
 
 const Playlists = ({ accessToken, onSelectPlaylist }) => {
   const [playlists, setPlaylists] = useState([]);
-  // eslint-disable-next-line
-  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
 
   const getCurrentUserId = useCallback(async () => {
-    if (localStorage.getItem("spotify_user_id")) {
-      return Promise.resolve(localStorage.getItem("spotify_user_id"));
-    } else {
-      const response = await fetch("https://api.spotify.com/v1/me", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      const userData = await response.json();
-      localStorage.setItem("spotify_user_id", userData.id);
-      return userData.id;
-    }
+    const storedUserId = localStorage.getItem("spotify_user_id");
+    if (storedUserId) return storedUserId;
+
+    const response = await fetch("https://api.spotify.com/v1/me", {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    const userData = await response.json();
+    localStorage.setItem("spotify_user_id", userData.id);
+    return userData.id;
   }, [accessToken]);
 
   const getUserPlaylists = useCallback(async () => {
@@ -26,20 +22,13 @@ const Playlists = ({ accessToken, onSelectPlaylist }) => {
       const userId = await getCurrentUserId();
       const response = await fetch(
         `https://api.spotify.com/v1/users/${userId}/playlists`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
+        { headers: { Authorization: `Bearer ${accessToken}` } }
       );
 
       if (response.ok) {
         const playlistsData = await response.json();
         setPlaylists(
-          playlistsData.items.map((playlist) => ({
-            playlistId: playlist.id,
-            name: playlist.name,
-          }))
+          playlistsData.items.map(({ id, name }) => ({ playlistId: id, name }))
         );
       } else {
         console.error("Error fetching user playlists:", response.status);
@@ -51,7 +40,6 @@ const Playlists = ({ accessToken, onSelectPlaylist }) => {
 
   useEffect(() => {
     getUserPlaylists();
-    // eslint-disable-next-line
   }, [getUserPlaylists]);
 
   const getPlaylist = async (playlistId) => {
@@ -59,21 +47,17 @@ const Playlists = ({ accessToken, onSelectPlaylist }) => {
       const userId = await getCurrentUserId();
       const response = await fetch(
         `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
+        { headers: { Authorization: `Bearer ${accessToken}` } }
       );
 
       if (response.ok) {
         const playlistData = await response.json();
-        return playlistData.items.map((track) => ({
-          id: track.track.id,
-          name: track.track.name,
-          artist: track.track.artists[0].name,
-          preview_url: track.track.preview_url,
-          duration_ms: track.track.duration_ms,
+        return playlistData.items.map(({ track }) => ({
+          id: track.id,
+          name: track.name,
+          artist: track.artists[0].name,
+          preview_url: track.preview_url,
+          duration_ms: track.duration_ms,
         }));
       } else {
         console.error("Error fetching playlist tracks:", response.status);
@@ -84,24 +68,24 @@ const Playlists = ({ accessToken, onSelectPlaylist }) => {
   };
 
   const selectPlaylist = async (playlistId) => {
-    const tracks = await getPlaylist(playlistId);
-    const selectedPlaylist = {
-      id: playlistId,
-      tracks: tracks,
-    };
-    setSelectedPlaylist(selectedPlaylist);
-    onSelectPlaylist(selectedPlaylist);
-    console.log("Selected Playlist:", selectedPlaylist);
+    try {
+      const tracks = await getPlaylist(playlistId);
+      const selectedPlaylist = { id: playlistId, tracks };
+      onSelectPlaylist(selectedPlaylist);
+      console.log("Selected Playlist:", selectedPlaylist);
+    } catch (error) {
+      console.error("Error selecting playlist:", error);
+    }
   };
 
   return (
     <div className="container mx-auto my-8 p-8 bg-gray-300 shadow-md rounded-md">
       <h2 className="text-2xl font-bold mb-4 text-[#003636]">Your Playlists</h2>
-      {playlists.map((playlist) => (
+      {playlists.map(({ playlistId, name }) => (
         <PlaylistsItem
-          key={playlist.playlistId}
-          playlistId={playlist.playlistId}
-          name={playlist.name}
+          key={playlistId}
+          playlistId={playlistId}
+          name={name}
           selectPlaylist={selectPlaylist}
           onSelectPlaylist={onSelectPlaylist}
         />
